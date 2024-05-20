a4dd9493f38fef024cd924e72e7e39c5e63edafd
ZarinaChernova <103840428+ZarinaChernova@users.noreply.github.com>
2024-05-20 11:39:02 +0300

    Добавила required в форму

--- web/form.php
+++ web/form.php
@@ -19,7 +19,7 @@
 
     <form action="post.php" method="post">
         <label for="username">Введите ваше имя:</label><br>
-        <input type="text" id="username" name="username"><br><br>
+        <input type="text" id="username" name="username" required><br><br>
         <input type="submit" value="Отправить">
     </form>
 
